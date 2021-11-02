fc3fd8749b25248c41272a6811e19e603b4e8b43
Marianna <mariannadek@gmail.com>
2021-11-02 18:23:52 +0300

    Задача №1.2 - 1.3 Заполнили массивы С и D

--- practicum/Program.cs
+++ practicum/Program.cs
@@ -1,36 +1,85 @@
-﻿int[] A = new int[10];
-int[] B = new int[10];
+﻿// Задача №1
+// Cформировать случайным образом целочисленный массив A
+// из натуральных двузначных чисел.
+
+// Создать на его основе масив B, отбрасывая те, которые
+// 1  нарушают порядок возрастания
+// 2  больше среднего арифметического элементов A
+// 3  чётные
+
+int[] A = new int[10];
+int[] B = new int[10]; // условие 1
+int[] C = new int[10]; // условие 2
+int[] D = new int[10]; // условие 3
 int index = 0;
 
-// Заполняем массив
+// Заполняем массив A
 while (index < 10)
 {
-    A[index] = new Random().Next(10,100); //[10,99]
+    A[index] = new Random().Next(10, 100); //[10,99]
     index++;
 }
 
 index = 0;
 
 // Печатаем массив
-
-while (index<10)
+Console.WriteLine("Массив A");
+while (index < 10)
 {
     Console.WriteLine(A[index]);
     index++;
 }
+
+
 Console.WriteLine();
+Console.WriteLine("Массив B");
+// Исключение элементов нарушения возрастания и заполнение массива B
 index = 1;
-// Исключение элементов нарушения возрастания 
 int currentEl = A[0];
 Console.WriteLine(currentEl);
-while (index<10)
+while (index < 10)
 {
-    if(A[index]>currentEl)
+    if (A[index] > currentEl)
     {
-        //Заполнение массива B
         B[index] = A[index];
         Console.WriteLine(B[index]);
         currentEl = A[index];
     }
     index++;
 }
+
+Console.WriteLine();
+//Поиск среднего арифметического элементов массива А
+int summ = 0;
+for (index = 0; index < A.Length; index++)
+    summ += A[index];
+int mid = summ / A.Length;
+
+Console.WriteLine();
+Console.WriteLine("Массив С");
+//Заполнение и вывод массива С элементами, меньшими или равными среднего арифметического элементов массива А
+index = 0;
+while (index < 10)
+{
+    if (A[index] <= mid)
+    {
+        C[index] = A[index];
+        Console.WriteLine(C[index]);
+    }
+    index++;
+}
+
+Console.WriteLine();
+Console.WriteLine("Массив D");
+//Заполнение и вывод массива D нечетными элементами массива A
+index = 0;
+while (index < 10)
+{
+    if (A[index] % 2 == 1)
+    {
+        D[index] = A[index];
+        Console.WriteLine(D[index]);
+    }
+    index++;
+}
+
